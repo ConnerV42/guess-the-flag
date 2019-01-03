17fb535c33159550514644a710d642f4f905f110
connerv42 <connerverret42@gmail.com>
2019-01-03 15:41:10 -0800

    add some css styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const style = {
+      backgroundImage: `url(/Globe.jpg)`
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="title-header" style={style}>
+          <h1 className="title-text">Guess The Flag</h1>
         </header>
       </div>
     );
